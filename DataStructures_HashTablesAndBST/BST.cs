6ece4b2a26a07fa699f94c5bf26e8a0ed66b5a78
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-27 17:31:26 +0530

    Add elements according to size in BinarySearchTree UC2_BSTAddElementUsingSize

--- DataStructures_HashTablesAndBST/BST.cs
+++ DataStructures_HashTablesAndBST/BST.cs
@@ -51,5 +51,9 @@ namespace DataStructures_HashTablesAndBST
                 this.RightTree.Display();
             }
         }
+        public void GetSize()
+        {
+            Console.WriteLine("Size"+" "+(1 +this.leftCount +this.rightCount));
+        }
     }
 }
--- DataStructures_HashTablesAndBST/Program.cs
+++ DataStructures_HashTablesAndBST/Program.cs
@@ -18,7 +18,18 @@ namespace DataStructures_HashTablesAndBST
             BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
             binarySearchTree.Insert(30);
             binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
             binarySearchTree.Display();
+            binarySearchTree.GetSize();
         }
         public static void CountNumbOfOccurence(string Paragraph)
         {
