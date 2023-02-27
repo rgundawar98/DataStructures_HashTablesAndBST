4c3845087e205833dac1ef1eee5caab57a399a6b
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-27 16:04:10 +0530

    Adding welcome message

---
+++ DataStructures_HashTablesAndBST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructures_HashTablesAndBST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the DataStructures HashTable and Binary Search Tree");
+        }
+    }
+}
